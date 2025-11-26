1c18bd5a3915edf37917fe75a611be144871c08e
lihibarsimantov <lihi.barsimantov@post.runi.ac.il>
2025-11-26 11:45:05 +0200

    Implement Sieve of Eratosthenes algorithm to find and display prime numbers up to a specified limit

--- Primes.java
+++ Primes.java
@@ -1,5 +1,32 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        // Sieve of Eratosthenes algorithm
+        final int N = Integer.parseInt(args[0]); // the upper limit
+        boolean[] isPrime = new boolean[N + 1];
+        for (int i = 2; i <= N; i++) {
+            isPrime[i] = true;// assume all numbers are prime from 2 to N
+        }
+        int p = 2;
+        // find all primes up to N (we can stop at sqrt(N))
+        while (p <= Math.sqrt(N)) {
+            int multiple = 2 * p;
+            while (multiple <= N) {
+                isPrime[multiple] = false; // mark multiples of p as non-prime
+                multiple += p;
+            }
+            p++;
+        }
+        System.out.println("Prime numbers up to " + N + ":");
+        // print all prime numbers found
+        int count = 0;
+        for (int i = 2; i <= N; i++) {
+            if (isPrime[i]) {
+                System.out.println(i);
+                count++;
+            }
+        }
+        int average = (int) (((double) count / (double) N) * 100.0);
+        System.out.println("There are " + count + " primes between 2 and " + N + " (" + average + "% are primes)");
+
     }
 }
